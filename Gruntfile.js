5dfc0f0ac0f57e7a1bc6cc3b4b3cf31491e0e44a
Piotrek-Sz <piotr.szyjka@gmail.com>
2016-02-16 21:12:16 +0100

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/style.css': 'scss/style.scss'
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['scss/**/*.scss'],
+                tasks: ['sass'],
+
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.registerTask('default', ['sass', 'watch']);
+
+};
